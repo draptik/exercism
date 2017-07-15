b6751849c51924f337949159cbb188ed2d19e167
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-15 15:59:33 +0200

    extracted Solution1

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -4,9 +4,15 @@ public static class Complement
 {
     public static string OfDna(string nucleotide)
     {
+        return Solution1(nucleotide);
+    }
+
+    private static string Solution1(string nucleotide)
+    {
         if (nucleotide == "G") return "C";
         if (nucleotide == "T") return "A";
         if (nucleotide == "A") return "U";
-        return "G";
+        if (nucleotide == "C") return "G";
+        throw new ArgumentOutOfRangeException(nameof(nucleotide));
     }
 }
