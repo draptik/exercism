692a2bb9f5024e0c99428ce7852c51145ed10887
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-19 00:03:09 +0200

    added simple algorithn

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
@@ -14,8 +15,22 @@ public static class SumOfMultiples
         return 0;
     }
 
+    private static int Convert02(IEnumerable<int> multiples, int max)
+    {
+        var numbers = new List<int>();
+        foreach (var currentMultiple in multiples)
+        {
+            for (int candidate = currentMultiple; candidate < max; candidate += currentMultiple)
+            {
+                numbers.Add(candidate);
+            }
+        }
+        return numbers.Distinct().Sum();
+    }
+    
     public static int To(IEnumerable<int> multiples, int max)
     {
-        return Convert01(multiples, max);
+        // return Convert01(multiples, max);
+        return Convert02(multiples, max);
     }
 }
