ce1a6b71a1e1872b7b586c49e6c4db1a58525f57
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-18 21:56:25 +0200

    applied pattern matching for single input string

--- fsharp/rna-transcription/RNATranscription.fs
+++ fsharp/rna-transcription/RNATranscription.fs
@@ -1,4 +1,17 @@
 module RNATranscription
 
+// let matchDna2Rna dna =
+//     match dna with
+//     | 'C' -> 'G'
+//     | 'G' -> 'C'
+//     | 'T' -> 'A'
+//     | 'A' -> 'U'
+//     | _ -> 'x'
+
 let toRna dnaNucleotid =
-    "G"
+    match dnaNucleotid with
+    | "C" -> "G"
+    | "G" -> "C"
+    | "T" -> "A"
+    | "A" -> "U"
+    | _ -> ""
--- fsharp/rna-transcription/RNATranscriptionTest.fs
+++ fsharp/rna-transcription/RNATranscriptionTest.fs
@@ -6,25 +6,20 @@ open RNATranscription
 
 [<Fact>]
 let ``Rna complement of cytosine is guanine`` () =
-    
-    Assert.That(toRna "C", Is.EqualTo("G"))
+    Assert.Equal("G", toRna "C")
 
 [<Fact>]
-[<Ignore("Remove to run test")>]
 let ``Rna complement of guanine is cytosine`` () =
-    Assert.That(toRna "G", Is.EqualTo("C"))
+    Assert.Equal("C", toRna "G")
 
 [<Fact>]
-[<Ignore("Remove to run test")>]
 let ``Rna complement of thymine is adenine`` () =
-    Assert.That(toRna "T", Is.EqualTo("A"))
+    Assert.Equal("A", toRna "T")
 
 [<Fact>]
-[<Ignore("Remove to run test")>]
 let ``Rna complement of adenine is uracil`` () =
-    Assert.That(toRna "A", Is.EqualTo("U"))
+    Assert.Equal("U", toRna "A")
 
 [<Fact>]
-[<Ignore("Remove to run test")>]
 let ``Rna complement`` () =
-    Assert.That(toRna "ACGTGGTCTTAA", Is.EqualTo("UGCACCAGAAUU"))
+    Assert.Equal("UGCACCAGAAUU", toRna "ACGTGGTCTTAA")
