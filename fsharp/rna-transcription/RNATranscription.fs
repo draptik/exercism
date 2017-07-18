f49ebcec4413203c35d5842268ec1105aefa6961
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-18 22:37:01 +0200

    using string.map with implicit signature

--- fsharp/rna-transcription/RNATranscription.fs
+++ fsharp/rna-transcription/RNATranscription.fs
@@ -8,6 +8,7 @@ let matchDna2Rna dna =
     | 'A' -> 'U'
     | _ -> 'x'
 
-// Using String.map with explicit signature
-let toRna (input:string) : string =
-    String.map matchDna2Rna input
+// Using String.map with implicit signature
+let toRna dna = 
+    String.map matchDna2Rna dna
+
