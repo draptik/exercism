0b575bf652a629dbd953a6fde3d22ea5dcb1b116
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-19 01:47:21 +0200

    removed Convert01

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -4,17 +4,6 @@ using System.Linq;
 
 public static class SumOfMultiples
 {
-    private static int Convert01(IEnumerable<int> multiples, int max)
-    {
-        if (max == 10000) return 2203160;
-        if (max == 1000) return 233168;
-        if (max == 100) return 2318;
-        if (max == 20) return 51;
-        if (max == 10) return 23;
-        if (max == 4) return 3;
-        return 0;
-    }
-    
     private static bool IsNumberDividableBy(int number, int dividableBy)
     {
         return number % dividableBy == 0;
@@ -45,7 +34,6 @@ public static class SumOfMultiples
     
     public static int To(IEnumerable<int> multiples, int max)
     {
-        // return Convert01(multiples, max);
         return Convert02(multiples, max);
     }
 }
