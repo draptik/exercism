d33e36de2d55a00bc122ff534601fc1c8319a388
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-09 22:37:29 +0100

    refactored tests to xunit

--- fsharp/sum-of-multiples/SumOfMultiplesTest.fs
+++ fsharp/sum-of-multiples/SumOfMultiplesTest.fs
@@ -1,33 +1,28 @@
 ﻿module SumOfMultiplesTest
 
-open NUnit.Framework
+open Xunit
 open SumOfMultiples
 
-[<Test>]
+[<Fact>]
 let ``Sum to 1`` () =
-    Assert.That(sumOfMultiples [3; 5] 0, Is.EqualTo(0))
+    Assert.Equal(sumOfMultiples [3; 5] 0, 0)
 
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sum to 3`` () =
-    Assert.That(sumOfMultiples [3; 5] 3, Is.EqualTo(0))
+// [<Fact>]
+// let ``Sum to 3`` () =
+//     Assert.Equal(sumOfMultiples [3; 5] 3, 0)
 
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sum to 4`` () =
-    Assert.That(sumOfMultiples [3; 5] 4, Is.EqualTo(3))
+// [<Fact>]
+// let ``Sum to 4`` () =
+//     Assert.Equal(sumOfMultiples [3; 5] 4, 3)
 
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sum to 10`` () =
-    Assert.That(sumOfMultiples [3; 5] 10, Is.EqualTo(23))
+// [<Fact>]
+// let ``Sum to 10`` () =
+//     Assert.Equal(sumOfMultiples [3; 5] 10, 23)
 
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sum to 20`` () =
-    Assert.That(sumOfMultiples [7; 13; 17] 20, Is.EqualTo(51))
+// [<Fact>]
+// let ``Sum to 20`` () =
+//     Assert.Equal(sumOfMultiples [7; 13; 17] 20, 51)
 
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sum to 10000`` () =
-    Assert.That(sumOfMultiples [43; 47] 10000, Is.EqualTo(2203160))
+// [<Fact>]
+// let ``Sum to 10000`` () =
+//     Assert.Equal(sumOfMultiples [43; 47] 10000, 2203160)
