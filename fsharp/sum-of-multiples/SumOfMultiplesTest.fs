5982ad0691990c8d477ffeb2862d2bae3f6810a2
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-09 22:53:22 +0100

    sum to 20 test fails

--- fsharp/sum-of-multiples/SumOfMultiplesTest.fs
+++ fsharp/sum-of-multiples/SumOfMultiplesTest.fs
@@ -19,9 +19,9 @@ let ``Sum to 4`` () =
 let ``Sum to 10`` () =
     Assert.Equal(sumOfMultiples [3; 5] 10, 23)
 
-// [<Fact>]
-// let ``Sum to 20`` () =
-//     Assert.Equal(sumOfMultiples [7; 13; 17] 20, 51)
+[<Fact>]
+let ``Sum to 20`` () =
+    Assert.Equal(sumOfMultiples [7; 13; 17] 20, 51)
 
 // [<Fact>]
 // let ``Sum to 10000`` () =
