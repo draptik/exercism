a4240451f24e3abd77edbbb3023d5677a53ff4ba
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 12:22:58 +0100

    renamed variables

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -4,8 +4,8 @@ let empty: Map<int, string list> =
     Map.empty
 
 let add (student: string) (grade: int) (school: Map<int, string list>): Map<int, string list> =
-    let result = school.TryFind grade
-    match result with
+    let schoolHasGrade = school.TryFind grade
+    match schoolHasGrade with
     | Some _ ->
         Map.map (fun _ value -> List.append [student] value) school
     | None ->
@@ -15,7 +15,7 @@ let roster (school: Map<int, string list>): (int * string list) list =
     school |> Map.toList
 
 let grade (number: int) (school: Map<int, string list>): string list =
-    let result = school.TryFind number
-    match result with
-    | Some x -> List.sort x
+    let schoolHasGrade = school.TryFind number
+    match schoolHasGrade with
+    | Some students -> List.sort students
     | None -> [""]
