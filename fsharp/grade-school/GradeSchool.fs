d00181b929d09437058316d422cae599556ee83b
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 12:19:54 +0100

    adding more students test passes

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -4,10 +4,28 @@ let empty: Map<int, string list> =
     Map.empty
 
 let add (student: string) (grade: int) (school: Map<int, string list>): Map<int, string list> =
-    Map.ofList [ (2, ["Aimee"]) ]
+    printfn "ADD --------------------------------"
+    printfn "ADD school before : %A" school
+
+    let result = school.TryFind grade
+    match result with
+    | Some mapEntry ->
+        printfn "ADD mapEntry : %A" mapEntry
+        Map.map (fun _ value -> List.append [student] value) school
+    | None ->
+        printfn "ADD NONE (adding student to grade) : %A" student
+        Map.add grade [student] school
+
+    // Map.ofList [ (2, ["Aimee"]) ]
 
 let roster (school: Map<int, string list>): (int * string list) list =
     school |> Map.toList
 
 let grade (number: int) (school: Map<int, string list>): string list =
-    ["Aimee"]
+    printfn "GRADE -----------------------------"
+    printfn "GRADE school : %A" school
+    let result = school.TryFind number
+    match result with
+    | Some x -> List.sort x
+    | None -> [""]
+    // ["Aimee"]
