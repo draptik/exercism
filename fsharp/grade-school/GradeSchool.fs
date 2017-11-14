7980230432a521eeaaafc5493e416e9ce5dfa6e1
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 11:32:22 +0100

    first test passes

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -1,9 +1,11 @@
 ﻿module GradeSchool
 
-let empty: Map<int, string list> = failwith "You need to implement this function."
+let empty: Map<int, string list> =
+    Map.empty
 
 let add (student: string) (grade: int) (school: Map<int, string list>): Map<int, string list> = failwith "You need to implement this function."
 
-let roster (school: Map<int, string list>): (int * string list) list = failwith "You need to implement this function."
+let roster (school: Map<int, string list>): (int * string list) list =
+    school |> Map.toList
 
 let grade (number: int) (school: Map<int, string list>): string list = failwith "You need to implement this function."
