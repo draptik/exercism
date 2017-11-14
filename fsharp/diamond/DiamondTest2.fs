d189b877d7befcff8570b3f18ae6a8fd9a4e1e7b
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 16:54:06 +0100

    extracted DiamondProperty

--- fsharp/diamond/DiamondTest2.fs
+++ fsharp/diamond/DiamondTest2.fs
@@ -5,7 +5,6 @@ module DiamondProperties
 open System
 open FsCheck
 open FsCheck.Xunit
-open FsCheck
 
 let make letter = "Devil's advocat"
 
@@ -15,10 +14,12 @@ type Letters =
         Arb.Default.Char()
         |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
 
+type DiamondPropertyAttribute () =
+    inherit PropertyAttribute(Arbitrary = [| typeof<Letters> |])
 
-[<Property(Arbitrary = [| typeof<Letters> |])>]
+[<DiamondProperty>]
 let ``Dimaond is non empty`` (letter: char) =
-    printfn "%c" letter
+    // printfn "%c" letter
     let actual = make letter
     not (String.IsNullOrWhiteSpace actual)
 
