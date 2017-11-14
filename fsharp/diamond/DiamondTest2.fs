1310b60604a27b8ad64e9b48f9d353db9a953e7b
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 16:49:35 +0100

    added Arbirtrary Letters

--- fsharp/diamond/DiamondTest2.fs
+++ fsharp/diamond/DiamondTest2.fs
@@ -3,22 +3,22 @@ module DiamondProperties
 // Following along Ploeh's Pluralsight course...
 
 open System
-// open FsCheck
+open FsCheck
 open FsCheck.Xunit
+open FsCheck
 
 let make letter = "Devil's advocat"
 
 
-// type Letters =
-//     static member Chars () =
-//         Arb.Default.Char()
-//         |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
+type Letters =
+    static member Chars () =
+        Arb.Default.Char()
+        |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
 
 
-// "Ice Breaker Test"
-[<Property>]
+[<Property(Arbitrary = [| typeof<Letters> |])>]
 let ``Dimaond is non empty`` (letter: char) =
-    // printfn "%c" letter
+    printfn "%c" letter
     let actual = make letter
     not (String.IsNullOrWhiteSpace actual)
 
