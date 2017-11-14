514ad0072222d4ccc7a42cc6547f100b6ec9018c
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 16:48:10 +0100

    implemented ice breaker test

--- fsharp/diamond/DiamondTest2.fs
+++ fsharp/diamond/DiamondTest2.fs
@@ -3,12 +3,22 @@ module DiamondProperties
 // Following along Ploeh's Pluralsight course...
 
 open System
+// open FsCheck
 open FsCheck.Xunit
 
 let make letter = "Devil's advocat"
 
+
+// type Letters =
+//     static member Chars () =
+//         Arb.Default.Char()
+//         |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
+
+
+// "Ice Breaker Test"
 [<Property>]
 let ``Dimaond is non empty`` (letter: char) =
+    // printfn "%c" letter
     let actual = make letter
     not (String.IsNullOrWhiteSpace actual)
 
