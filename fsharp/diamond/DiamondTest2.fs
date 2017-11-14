f32a508d3f530ea8daad9b49fdc89e4ac9b541ad
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-15 00:15:39 +0100

    refactor: removed branching for special case 'A'

--- fsharp/diamond/DiamondTest2.fs
+++ fsharp/diamond/DiamondTest2.fs
@@ -10,17 +10,13 @@ let make letter =
     let makeLine letterCount (letter, letterIndex) =
         let leadingSpace = String(' ', letterCount - 1 - letterIndex)
         let innerSpace = String(' ', letterCount - 1 - leadingSpace.Length)
-        match letter with
-        | 'A' ->
-            sprintf "%s%c%s" leadingSpace letter leadingSpace
-        | _ ->
-            let left = 
-                sprintf "%s%c%s" leadingSpace letter innerSpace 
-                |> Seq.toList
-            left
-            @ (left |> List.rev |> List.tail)
-            |> List.map string
-            |> List.reduce (sprintf "%s%s")
+        let left = 
+            sprintf "%s%c%s" leadingSpace letter innerSpace 
+            |> Seq.toList
+        left
+        @ (left |> List.rev |> List.tail)
+        |> List.map string
+        |> List.reduce (sprintf "%s%s")
 
     let letters = ['A' .. letter] |> List.mapi (fun i l -> l, i)
 
