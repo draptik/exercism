b3aa43894207ca18f82dfdaa484203cdfe50bf04
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-14 12:36:54 +0100

    roster now sorts student names

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -12,7 +12,9 @@ let add (student: string) (grade: int) (school: Map<int, string list>): Map<int,
         Map.add grade [student] school
 
 let roster (school: Map<int, string list>): (int * string list) list =
-    school |> Map.toList
+    school 
+    |> Map.map (fun _ v -> List.sort v) 
+    |> Map.toList
 
 let grade (number: int) (school: Map<int, string list>): string list =
     let schoolHasGrade = school.TryFind number
