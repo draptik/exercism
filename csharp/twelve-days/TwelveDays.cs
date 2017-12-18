281c19a6b15658d427219eaa6d49d23357554f37
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-18 20:39:17 +0100

    enumerated christmas day

--- csharp/twelve-days/TwelveDays.cs
+++ csharp/twelve-days/TwelveDays.cs
@@ -11,19 +11,38 @@ public static class TwelveDaysSong
     {
         if (verseNumber == 3)
         {
-            return "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+            return $"On the {EnumeratedChristmasDay(verseNumber)} day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
         }
         
         if (verseNumber == 2)
         {
-            return "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+            return $"On the {EnumeratedChristmasDay(verseNumber)} day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n";
         }
         
-        return "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n";
+        return $"On the {EnumeratedChristmasDay(verseNumber)} day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n";
     }
 
     public static string Verses(int start, int end)
     {
         throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private static string EnumeratedChristmasDay(int day) 
+    {
+        switch (day)
+        {
+            case 1: return "first";
+            case 2: return "second";
+            case 3: return "third";
+            case 4: return "fourth";
+            case 5: return "fifth";
+            case 6: return "sixth";
+            case 7: return "seventh";
+            case 8: return "eight";
+            case 9: return "ninth";
+            case 10: return "tenth";
+            case 11: return "eleventh";
+            default: throw new Exception("Not a christmas day, you grunch!");
+        }
     }
 }
