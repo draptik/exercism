e6817ad850107990c1efad52c41a8ed167b88372
Patrick Drechsler <socialcoding@dprechsler.de>
2017-12-20 17:05:19 +0100

    ported code from fsx to fs (doesn't pass test yet)

--- fsharp/twelve-days/TwelveDays.fs
+++ fsharp/twelve-days/TwelveDays.fs
@@ -2,22 +2,43 @@
 
 let days = ["first"; "second"; "third"; "fourth"; "fifth"; "sixth"; "seventh"; "eighth"; "ninth"; "tenth"; "eleventh"; "twelfth"]
 
+
 let songlines = [
-    (12, "twelve Drummers Drumming"), 
-    (11, "eleven Pipers Piping"), 
-    (10, "ten Lords-a-Leaping"), 
-    (9, "nine Ladies Dancing"), 
-    (8, "eight Maids-a-Milking"),
-    (7, "seven Swans-a-Swimming"), 
-    (6, "six Geese-a-Laying"), 
-    (5, "five Gold Rings"), 
-    (4, "four Calling Birds"), 
-    (3, "three French Hens"), 
-    (2, "two Turtle Doves"), 
+    (12, "twelve Drummers Drumming"); 
+    (11, "eleven Pipers Piping"); 
+    (10, "ten Lords-a-Leaping"); 
+    (9, "nine Ladies Dancing"); 
+    (8, "eight Maids-a-Milking");
+    (7, "seven Swans-a-Swimming"); 
+    (6, "six Geese-a-Laying"); 
+    (5, "five Gold Rings"); 
+    (4, "four Calling Birds"); 
+    (3, "three French Hens"); 
+    (2, "two Turtle Doves"); 
     (1, "and a Partridge in a Pear Tree.")
 ]
 
-let verse day =
+let introSentence day =
     "On the " + days.[day - 1] + " day of Christmas my true love gave to me"
 
+let modifyFirstDay (line:string, day) =
+    match day with
+    | 1 -> line.Replace("and ", "")
+    | _ -> line
+
+let getItems day =
+    songlines
+    |> List.filter(fun x -> (fst(x) <= day))
+    |> List.sortBy(fun x -> (fst(x)))
+    |> List.rev
+    |> List.map(fun x -> snd(x))
+    |> List.map(fun x -> modifyFirstDay(x, day))
+    |> List.reduce(fun content i -> content + ", " + i)
+
+let verse day =
+    introSentence day + ", " + getItems day
+
 let recite start stop =
+    [start..stop]
+    |> List.map(verse)
+    |> List.reduce(fun x y -> x + "\n"+ y)
