5a37836361a2e4e1c3da654235327e3146843825
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-20 22:34:33 +0100

    too stupid for javascript

--- javascript/twelve-days/twelve-days.js
+++ javascript/twelve-days/twelve-days.js
@@ -1,3 +1,9 @@
-var TwelveDays = function(){};
+var TwelveDays = function() {
+
+    verse = function (arrVersesRange) {
+        return 'On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n';
+    }
+
+};
 
 module.exports = TwelveDays;
