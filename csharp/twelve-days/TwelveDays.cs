85ef6f41ac105deb34fb530eda861ffd6fe5fef3
Patrick Drechsler <socialcoding@dprechsler.de>
2017-12-20 09:53:55 +0100

    added namespace and fixed first test

--- csharp/twelve-days/TwelveDays.cs
+++ csharp/twelve-days/TwelveDays.cs
@@ -2,82 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public static class TwelveDaysSong
+namespace TwelveDays
 {
-    public static string Sing()
+    public static class TwelveDaysSong
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public static string Verse(int verseNumber)
-    {
-        // Console.WriteLine("Hello");
-        // var result = GetItems(verseNumber);
-        // Console.WriteLine(result);
-        // return result;
-        if (verseNumber == 3)
+        public static string Sing()
         {
-            return $"{IntroSentence(verseNumber)}, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+            throw new NotImplementedException("You need to implement this function.");
         }
-        
-        if (verseNumber == 2)
+
+        public static string Verse(int verseNumber)
         {
-            return $"{IntroSentence(verseNumber)}, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-        }
+            return $"{IntroSentence(verseNumber)}, {GetItems(verseNumber)}\n";
+            if (verseNumber == 3)
+            {
+                return $"{IntroSentence(verseNumber)}, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+            }
         
-        return $"{IntroSentence(verseNumber)}, a Partridge in a Pear Tree.\n";
-    }
+            if (verseNumber == 2)
+            {
+                return $"{IntroSentence(verseNumber)}, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+            }
+        
+            return $"{IntroSentence(verseNumber)}, a Partridge in a Pear Tree.\n";
+        }
 
-    public static string Verses(int start, int end)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        public static string Verses(int start, int end)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
 
-    private static string GetItems(int verseNumber)
-    {
-        return songLines
+        private static string GetItems(int verseNumber)
+        {
+            return SongLines
                 .Where(x => x.Key <= verseNumber)
                 .OrderBy(x => x.Key)
                 .Reverse()
                 .Select(x => x.Value)
                 .Aggregate((allItems, next) => allItems + next);
-    }
-    private static Dictionary<int, string> songLines = new Dictionary<int, string>{
-        {12, "twelve Drummers Drumming"}, 
-        {11, "eleven Pipers Piping"}, 
-        {10, "ten Lords-a-Leaping"}, 
-        {9, "nine Ladies Dancing"}, 
-        {8, "eight Maids-a-Milking"},
-        {7, "seven Swans-a-Swimming"}, 
-        {6, "six Geese-a-Laying"}, 
-        {5, "five Gold Rings"}, 
-        {4, "four Calling Birds"}, 
-        {3, "three French Hens"}, 
-        {2, "two Turtle Doves"}, 
-        {1, "and a Partridge in a Pear Tree."}
-    };
+        }
 
-    private static string IntroSentence(int verseNumber)
-    {
-        return $"On the {EnumeratedChristmasDay(verseNumber)} day of Christmas my true love gave to me";
-    }
+        private static readonly Dictionary<int, string> SongLines = new Dictionary<int, string>{
+            {12, "twelve Drummers Drumming"}, 
+            {11, "eleven Pipers Piping"}, 
+            {10, "ten Lords-a-Leaping"}, 
+            {9, "nine Ladies Dancing"}, 
+            {8, "eight Maids-a-Milking"},
+            {7, "seven Swans-a-Swimming"}, 
+            {6, "six Geese-a-Laying"}, 
+            {5, "five Gold Rings"}, 
+            {4, "four Calling Birds"}, 
+            {3, "three French Hens"}, 
+            {2, "two Turtle Doves"}, 
+            {1, "a Partridge in a Pear Tree."}
+        };
 
-    private static string EnumeratedChristmasDay(int day) 
-    {
-        switch (day)
+        private static string IntroSentence(int verseNumber)
         {
-            case 1: return "first";
-            case 2: return "second";
-            case 3: return "third";
-            case 4: return "fourth";
-            case 5: return "fifth";
-            case 6: return "sixth";
-            case 7: return "seventh";
-            case 8: return "eight";
-            case 9: return "ninth";
-            case 10: return "tenth";
-            case 11: return "eleventh";
-            default: throw new Exception("Not a christmas day, you grunch!");
+            return $"On the {EnumeratedChristmasDay(verseNumber)} day of Christmas my true love gave to me";
+        }
+
+        private static string EnumeratedChristmasDay(int day) 
+        {
+            switch (day)
+            {
+                case 1: return "first";
+                case 2: return "second";
+                case 3: return "third";
+                case 4: return "fourth";
+                case 5: return "fifth";
+                case 6: return "sixth";
+                case 7: return "seventh";
+                case 8: return "eight";
+                case 9: return "ninth";
+                case 10: return "tenth";
+                case 11: return "eleventh";
+                default: throw new Exception("Not a christmas day, you grunch!");
+            }
         }
     }
 }
--- csharp/twelve-days/TwelveDaysTest.cs
+++ csharp/twelve-days/TwelveDaysTest.cs
@@ -4,33 +4,34 @@ using System.Linq;
 using FluentAssertions;
 using Xunit;
 
-public class TwelveDaysTest
+namespace TwelveDays
 {
-    [Fact]
-    public void Return_verse_1()
+    public class TwelveDaysTest
     {
-        var expected = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n";
-        var actual = TwelveDaysSong.Verse(1);
-        Console.WriteLine(actual);
-        actual.Should().Be(expected);
+        [Fact]
+        public void Return_verse_1()
+        {
+            var expected = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n";
+            var actual = TwelveDaysSong.Verse(1);
+            Console.WriteLine(actual);
+            actual.Should().Be(expected);
+        }
+
+        // [Fact]
+        // public void Return_verse_2()
+        // {
+        //     var expected = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        //     var actual = TwelveDaysSong.Verse(2);
+        //     Console.WriteLine(actual);
+        //     actual.Should().Be(expected);
+        // }
+
+        [Fact]
+        public void SomeTest()
+        {
+            var list = new List<string>{"A", "B"};
+            var result = list.Aggregate((a, b) => a + ", " + b);
+            result.Should().Be("A, B");
+        }
     }
-
-    // [Fact]
-    // public void Return_verse_2()
-    // {
-    //     var expected = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-    //     var actual = TwelveDaysSong.Verse(2);
-    //     Console.WriteLine(actual);
-    //     actual.Should().Be(expected);
-    // }
-
-    [Fact]
-    public void SomeTest()
-    {
-        var emptyList = new List<string>();
-        var list = new List<string>{"a", "b"};
-        var result = emptyList.Aggregate((a, b) => a + b);
-        result.Should().Be("xxxxxxxxxxxxxxxxxx");
-    }
-
 }
