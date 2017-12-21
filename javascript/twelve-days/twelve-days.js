153c8810f03b342261aa593c578ad0fc2181959c
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-21 11:27:28 +0100

    fixed test setup

--- javascript/twelve-days/twelve-days.js
+++ javascript/twelve-days/twelve-days.js
@@ -1,9 +1,13 @@
-var TwelveDays = function() {
 
-    verse = function (arrVersesRange) {
-        return 'On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n';
+class TwelveDays {
+
+    verse(arrVersesRange) {
+        return 'On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n' + this.foo();
     }
 
+    foo() {
+        return "";
+    }
 };
 
 module.exports = TwelveDays;
