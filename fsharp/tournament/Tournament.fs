59401a7ba64873d99c670e59fc82c8dae9187c8f
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-29 00:27:01 +0100

    moved some ideas from fsx to fs

--- fsharp/tournament/Tournament.fs
+++ fsharp/tournament/Tournament.fs
@@ -1,5 +1,33 @@
 ﻿module Tournament
-    
+
+type Team = 
+    {   
+        Name: string
+        MatchesPlayed: int
+        MatchesWon: int
+        MatchesLost: int
+        MatchesDrawn: int
+        Points: int }
+
+let emptyTeamTemplate =
+    {   
+        Name = ""
+        MatchesPlayed = 0
+        MatchesWon = 0
+        MatchesLost = 0
+        MatchesDrawn = 0
+        Points = 0 }
+
+type GameOutcome = Win | Loss | Draw
+
+let parseOutcome outcome =
+    match outcome with
+    | "win" -> Win
+    | "loss" -> Loss
+    | "draw" -> Draw
+    | _ -> failwith "Invalid outcome (valid outcomes are 'win', 'loss' and 'draw')."
+
+
 let tally input =
     match input with
     | [] -> ["Team                           | MP |  W |  D |  L |  P"]
