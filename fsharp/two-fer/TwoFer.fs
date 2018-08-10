61e671312fac69f55dbdc84e943e67147ed9a5b8
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-10 16:46:44 +0200

    exercise two-fer f#

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,3 +1,6 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+let twoFer (input: string option): string =
+    match input with
+    | Some x -> "One for " + x + ", one for me."
+    | _ -> "One for you, one for me."
