7a0bca0755f4f7110c3f650c4e25a0a54d06548c
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-16 23:13:02 +0200

    space-age: 1st iteration

--- fsharp/space-age/SpaceAge.fs
+++ fsharp/space-age/SpaceAge.fs
@@ -1,5 +1,27 @@
 ﻿module SpaceAge
 
-// TODO: define the Planet type
+type Planet =
+    | Mercury
+    | Venus
+    | Earth
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
 
-let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
+let seconds2EarthYears sec = sec / 31557600.0
+
+let convertToEarthYears planet earthYears =
+    match planet with
+    | Mercury -> earthYears / 0.2408467
+    | Venus -> earthYears / 0.61519726
+    | Earth -> earthYears / 1.0
+    | Mars -> earthYears / 1.8808158
+    | Jupiter -> earthYears / 11.862615
+    | Saturn -> earthYears / 29.447498
+    | Uranus -> earthYears / 84.016846
+    | Neptune -> earthYears / 164.79132
+
+let age (planet: Planet) (seconds: int64): float =
+    seconds2EarthYears (float seconds) |> convertToEarthYears planet
