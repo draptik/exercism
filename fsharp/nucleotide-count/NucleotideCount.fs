6a681fbf69cc6fa4d4639cdbf22237c1bb455f82
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-16 18:22:56 +0200

    reformat

--- fsharp/nucleotide-count/NucleotideCount.fs
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -1,18 +1,21 @@
 ﻿module NucleotideCount
 
+// I don't understand how to use this solution:
+//
 // // Usage: count 'a' "aba" (returns 2)
 // let count x = Seq.filter ((=) x) >> Seq.length
-
+//
 // // Usage: count2 'a' "aba" (returns 2)
 // let count2 x xs =
 //     xs
 //     |> Seq.filter (fun x' -> x' = x)
 //     |> Seq.length
-
+//
 // let count3 xs x =
 //     xs
 //     |> Seq.filter (fun x' -> x' = x)
 //     |> Seq.length
+
 let nucleotides = ['A';'C';'T';'G']
 
 let emptyMap = 
@@ -20,7 +23,7 @@ let emptyMap =
     |> List.map (fun n -> (n,0)) 
     |> Map.ofList
 
-let nucleotideCounts' strand = 
+let nucleotideCountsInner strand = 
     strand
     |> Seq.countBy id
     |> Seq.fold 
@@ -30,5 +33,5 @@ let nucleotideCounts' strand =
 let isValid chars s = String.forall (fun c -> List.contains c chars) s
 
 let nucleotideCounts (strand: string): Option<Map<char, int>> =
-  if isValid nucleotides strand then nucleotideCounts' strand |> Some
+  if isValid nucleotides strand then nucleotideCountsInner strand |> Some
   else None
