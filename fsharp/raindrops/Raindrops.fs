54b57f222b331615091f0fedef85c0abfa78ed29
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-18 17:03:00 +0200

    raindrops: 1st iteration

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,3 +1,34 @@
 ﻿module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+let factors number = seq {
+    for divisor in 1 .. (float >> sqrt >> int) number do
+    if number % divisor = 0 then
+        yield divisor
+
+        //special case condition: when number=1 then divisor=(number/divisor), so don't repeat it
+        if number <> 1 then yield number / divisor 
+}
+
+let applyRule num =
+    match num = 3, num = 5, num = 7 with
+    | true, _, _ -> Some "Pling"
+    | _, true, _ -> Some "Plang"
+    | _, _, true -> Some "Plong"
+    | _ -> None
+
+let replace numbers =
+    let sound =
+        numbers
+        |> Seq.map applyRule
+        |> Seq.choose id
+        |> Seq.distinct
+        |> String.concat ""
+    if sound = "" then
+        let maxNumber = Seq.max numbers
+        maxNumber.ToString()
+    else
+        sound
+
+let convert (number: int): string =
+    factors number
+    |> replace
