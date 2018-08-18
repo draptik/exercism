ba35bf798484223951a8819e207c64d70a49890e
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-18 22:47:36 +0200

    first iteration of etl kata in f#

--- fsharp/etl/Etl.fs
+++ fsharp/etl/Etl.fs
@@ -1,3 +1,24 @@
 ﻿module Etl
 
-let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = failwith "You need to implement this function."
+let convertListOfCharsToLowerCase (xs: char list) =
+    xs
+    |> Seq.map (fun x -> (string x).ToLower())
+    |> Seq.map (fun x -> (char x))
+
+let convertOldEntry oldEntry =
+    let score, chars = oldEntry
+    chars 
+    |> convertListOfCharsToLowerCase
+    |> Seq.map (fun c -> (c, score))
+
+let convertEntries xs =
+    xs
+    |> Seq.collect convertOldEntry
+    |> Seq.sortBy (fun (c, _) -> c)
+    |> List.ofSeq
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> =
+    scoresWithLetters
+    |> Map.toSeq
+    |> convertEntries
+    |> Map.ofList    
