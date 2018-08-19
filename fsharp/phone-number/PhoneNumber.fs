a4a7f4cb2a2e9b72457ff6e08094e83835082c8e
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-19 02:09:25 +0200

    area code and exchange code is still red

--- fsharp/phone-number/PhoneNumber.fs
+++ fsharp/phone-number/PhoneNumber.fs
@@ -1,3 +1,24 @@
 ﻿module PhoneNumber
 
-let clean input = failwith "You need to implement this function."
+open System.Text.RegularExpressions
+
+let stripNonNumbers s =
+    Regex.Replace(s, "[^0-9.]", "")
+
+let stripDots s =
+    Regex.Replace(s, "\.", "")
+
+let stripLeadingUSCountryCode (s:string) =
+    if s.Length = 11 && s.[0] = '1' then s.[1..10]
+    else s
+
+let validateLength (s:string) =
+    if s.Length = 10 then Some s
+    else None
+
+let clean input =
+    input
+    |> stripNonNumbers
+    |> stripDots
+    |> stripLeadingUSCountryCode
+    |> validateLength
