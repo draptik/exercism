80cb0f8492e590897ea69b3800f2237458dddc92
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-08-20 22:01:46 +0200

    raindrop refactoring after mentor feedback

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,34 +1,22 @@
 ﻿module Raindrops
 
-let factors number = seq {
-    for divisor in 1 .. (float >> sqrt >> int) number do
-    if number % divisor = 0 then
-        yield divisor
-
-        //special case condition: when number=1 then divisor=(number/divisor), so don't repeat it
-        if number <> 1 then yield number / divisor 
-}
-
-let applyRule num =
-    match num = 3, num = 5, num = 7 with
-    | true, _, _ -> Some "Pling"
-    | _, true, _ -> Some "Plang"
-    | _, _, true -> Some "Plong"
-    | _ -> None
-
-let replace numbers =
-    let sound =
-        numbers
-        |> Seq.map applyRule
-        |> Seq.choose id
-        |> Seq.distinct
-        |> String.concat ""
-    if sound = "" then
-        let maxNumber = Seq.max numbers
-        maxNumber.ToString()
-    else
-        sound
-
 let convert (number: int): string =
-    factors number
-    |> replace
+    let genStr divisor str =
+        fun (value, acc) -> 
+            if value % divisor = 0 then (value, acc + str)
+            else (value, acc)   
+
+    let pling = genStr 3 "Pling"
+    let plang = genStr 5 "Plang"
+    let plong = genStr 7 "Plong"
+
+    let extract x =
+        match x with
+        | (num, "") -> num.ToString()
+        | (_, sound) -> sound
+
+    (number, "")
+    |> pling
+    |> plang
+    |> plong
+    |> extract   
