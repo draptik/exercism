81f00ce1c7ec1706e8c99b70fa1a8b57f1c7c7f5
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-24 15:40:34 +0200

    fsharp: armstrong numbers v1

--- fsharp/armstrong-numbers/ArmstrongNumbers.fs
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -1,3 +1,21 @@
 ﻿module ArmstrongNumbers
+open System
 
-let isArmstrongNumber (number: int): bool = failwith "You need to implement this function."
+let isArmstrongNumber (number: int): bool =
+    
+    let digits = number.ToString() |> Seq.map (fun x -> Int32.Parse(x.ToString()))
+    let numberOfDigits = number.ToString().Length
+    
+    let powers = seq {
+        for d in digits -> (float d) ** (float numberOfDigits)    
+        }
+    
+    let sumOfPowers = Seq.sum powers
+    
+    let isArmstrong n = sumOfPowers = n
+        
+    match number with
+    | x when x < 10 && x > 0 -> true
+    | x when x >= 10 && x < 100 -> false
+    | x -> isArmstrong (float x)
+    
