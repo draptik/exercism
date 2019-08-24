6d49140ec0555765a21f6503498c2e47b80e6da8
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-24 15:16:15 +0200

    fsharp: collatz conjecture v1

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,16 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let steps (number: int): int option =
+
+    let isEven n = n % 2 = 0
+
+    let calcNewNum num =
+        if isEven num then num / 2
+        else (num * 3) + 1
+        
+    let rec stepsRec num stepCnt =
+        if num = 1 then stepCnt
+        else stepsRec (calcNewNum num) (stepCnt + 1)
+
+    if number <= 0 then None
+    else Some (stepsRec number 0)    
