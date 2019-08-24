51e8bcb46f23e552de8280403e238cd585a70c4c
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-24 13:56:30 +0200

    fsharp: queen attack v1

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,5 +1,14 @@
 ﻿module QueenAttack
+open System
 
-let create (position: int * int) = failwith "You need to implement this function."
+let create (position: int * int) =
+    match position with
+    | (x,y) when x > 0 && x < 8 && y > 0 && y < 8 -> true
+    | _ -> false
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let canAttack (queen1: int * int) (queen2: int * int) =
+    match queen1, queen2 with
+    | ((x1, _), (x2, _)) when x1 = x2 -> true
+    | ((_, y1), (_, y2)) when y1 = y2 -> true
+    | ((x1, y1), (x2, y2)) -> Math.Abs(x1 - x2) = Math.Abs(y1 - y2)
+    | _ -> false
