2a4a42c0a51ef9d7d8b5489ed6bb1529c4aff1a2
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-24 20:37:20 +0200

    fsharp: darts v1

--- fsharp/darts/Darts.fs
+++ fsharp/darts/Darts.fs
@@ -1,3 +1,11 @@
 ﻿module Darts
 
-let score (x: double) (y: double): int = failwith "You need to implement this function."
+let distance (x : double, y : double) = sqrt ((x * x) + (y * y))
+
+let score (x: double) (y: double): int =
+    match distance (x,y) with
+    | x when x <= 1.0 -> 10
+    | x when x <= 5.0 -> 5
+    | x when x <= 10.0 -> 1
+    | _ -> 0
+    
