b4837625bdf1505cae54509fc166daf501ba1f14
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-24 20:47:59 +0200

    fsharp: triangle kata

---
+++ fsharp/triangle/Triangle.fs
@@ -0,0 +1,8 @@
+﻿module Triangle
+
+let equilateral triangle =
+    true
+
+let isosceles triangle = failwith "You need to implement this function."
+
+let scalene triangle = failwith "You need to implement this function."
