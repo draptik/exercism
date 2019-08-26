34aa19afc2fe76dba40d23fdaed9a1a12402cfb7
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-26 21:45:17 +0200

    tmp

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -1,3 +1,21 @@
 ﻿module Pangram
 
-let isPangram (input: string): bool = failwith "You need to implement this function."
+open System
+open System.Text.RegularExpressions
+
+let allValidChars = [ 'a'..'z' ]
+
+let reduceToLowerCase coll =
+    coll
+    |> Seq.map(fun x -> x.ToString().ToLower())
+
+let removeNonLowerAlphaChars l =
+    l
+    |> Seq.map(fun x ->
+        match Regex.Match(x, "/[a-z]/)") with
+        | x -> "a"
+        | _ -> "b")
+
+
+let isPangram (input: string): bool =
+    true
