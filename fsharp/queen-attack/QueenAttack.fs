ba4a255796047408e85a35b50755bb406f191386
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-26 23:14:20 +0200

    fsharp: queen attack v3 - add deconstruct to canAttack

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -2,17 +2,11 @@
 open System
 
 let create (position: int * int) =
-
-    let internal_create (x:int, y: int) =
-        x > 0 && x < 8 && y > 0 && y < 8
-
-    internal_create position
+    let (x, y) = position
+    x > 0 && x < 8 && y > 0 && y < 8    
 
 let canAttack (queen1: int * int) (queen2: int * int) =
-
-    let internal_canAttack (x1:int, y1:int) (x2:int, y2:int) =
-        if x1 = x2 then true
-        else if y1 = y2 then true
-        else Math.Abs(x1 - x2) = Math.Abs(y1 - y2)
-
-    internal_canAttack queen1 queen2
+    let ((x1, y1), (x2, y2)) = (queen1, queen2)
+    if x1 = x2 then true
+    else if y1 = y2 then true
+    else Math.Abs(x1 - x2) = Math.Abs(y1 - y2)
