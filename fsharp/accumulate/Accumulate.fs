91980ac6651cb1befe5bf6216d13f12c8b991a73
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-27 02:31:05 +0200

    fsharp: accumulate v4

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,10 +1,30 @@
 ﻿module Accumulate
+
+// copied from:
+// https://blog.ploeh.dk/2015/12/22/tail-recurse/
+
+let rec mapNotTailRecursive f = function
+    | [] -> []
+    | h::t -> f h :: mapNotTailRecursive f t
+
+let mapTailRecursiveUsingAppend f xs =
+    
+    let rec mapImp f acc = function
+        | [] -> acc
+        | h::t -> mapImp f (acc @ [f h]) t
+    
+    mapImp f [] xs
+
+let mapTailRecursiveUsingRev f xs =
+    let rec mapImp f acc = function
+        | [] -> acc
+        | h::t -> mapImp f (f h :: acc) t
+    mapImp f [] xs |> List.rev
     
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
     
-    let rec acc f items =
-        match items with
-        | [] -> []
-        | head :: tail -> (f head) :: (acc f tail)
-    
-    acc func input
+    // TODO: understand / debug / performance test difference approaches        
+    input
+//    |> mapNotTailRecursive func 
+//    |> mapTailRecursiveUsingAppend func 
+    |> mapTailRecursiveUsingRev func
