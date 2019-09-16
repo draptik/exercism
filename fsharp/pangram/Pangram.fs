c8a87366cf5ab60ff7f69a6ffeb0b1b8999960c9
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-16 18:09:42 +0200

    fsharp: pangram solution

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -1,21 +1,9 @@
 ﻿module Pangram
 
-open System
-open System.Text.RegularExpressions
-
-let allValidChars = [ 'a'..'z' ]
-
-let reduceToLowerCase coll =
-    coll
-    |> Seq.map(fun x -> x.ToString().ToLower())
-
-let removeNonLowerAlphaChars l =
-    l
-    |> Seq.map(fun x ->
-        match Regex.Match(x, "/[a-z]/)") with
-        | x -> "a"
-        | _ -> "b")
-
+let allValidChars = set [ 'a'..'z' ]
 
 let isPangram (input: string): bool =
-    true
+    input.ToLowerInvariant()
+    |> set // <-- convert to Set
+    |> Set.isSubset allValidChars
+    
