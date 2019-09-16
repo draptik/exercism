56ac924dee20c9fd95d8bc92d80bea9f5c746d0c
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-16 20:24:03 +0200

    fsharp: isogram v1

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,4 +1,17 @@
 ﻿module Isogram
 
+open System
+
+let isIsogramInternalV1 (str : string) =
+    str
+    |> Seq.map Char.ToLowerInvariant // map all strings to lowercase char array
+    |> Seq.filter Char.IsLetter // we only care about letters
+    |> Seq.toList
+    |> List.groupBy id // group by identity (duplicates are grouped: "aba" -> [('a', ['a'; 'a']); ('b', ['b'])])
+    |> List.map snd // we only care about duplicate entries (array within array): "aba" -> [['a'; 'a']; ['b']]
+    |> List.filter (fun set -> set.Length > 1) // strip all non-duplicates: "aba" -> [['a'; 'a']]
+    |> List.map (fun set -> set.[0]) // only take the first entry
+    |> List.length = 0 // length > 0 means we have duplicates (not an isogram)
+
 let isIsogram str =
-    true
+    isIsogramInternalV1 str
