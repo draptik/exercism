cb199d56c4dd8695087a258c1a1ad378f1a2ec6c
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-16 19:22:52 +0200

    fsharp: robot name

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -1,7 +1,29 @@
 ﻿module RobotName
 
-let mkRobot() = failwith "You need to implement this function."
+open System
 
-let name robot = failwith "You need to implement this function."
+let rand = new System.Random()
 
-let reset robot = failwith "You need to implement this function."
+let generateRandomStringOf3Numbers (r : Random) =
+    r.Next(1000) // min: 0, max: 999
+    |> sprintf "%03i" // left pad with zeros
+
+// https://stackoverflow.com/a/33316100/1062607
+let shuffleR (r : Random) xs =
+    xs
+    |> Seq.sortBy (fun _ -> r.Next())
+
+let generate2RandomUpperCaseLetters (r : Random) =
+    seq [ 'A'..'Z' ] // create a sequence of chars
+    |> shuffleR r // shuffle the sequence
+    |> Seq.take 2 // take the first N entries from the shuffled sequence
+    |> Seq.map string // convert to list of strings
+    |> Seq.reduce (+) // aggregate
+
+let mkRobot() =
+    generate2RandomUpperCaseLetters rand + generateRandomStringOf3Numbers rand
+    |> sprintf "%s"
+
+let name robot = robot
+
+let reset robot = mkRobot()
