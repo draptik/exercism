f81768e7486d2966dbb6fc11e6e3ed7ee07786ff
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-19 20:09:20 +0200

    fsharp: protein translation - map to protein

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,4 +1,49 @@
 ﻿module ProteinTranslation
+
+open System
+
+type DomainError =
+    | InvalidRna of string
     
+module Rna =
+    
+    type ValidRna = private ValidRna of string
+
+    let hasExactly3Letters (s:string) : Result<string, DomainError> =
+        if s.Length = 3 then Ok s
+        else Error (InvalidRna "3 letters")
+    
+    let has3ValidLetters (s:string) =
+        let validLetters = set ['A'; 'C'; 'G'; 'T'; 'U']
+        if Set.isSuperset validLetters (set s) then Ok s
+        else Error (InvalidRna "invalid letter")
+        
+    let create (s:string) : Result<ValidRna, DomainError> =
+        if String.IsNullOrWhiteSpace(s) then
+            Error (InvalidRna "empty")
+        else
+            s
+            |> hasExactly3Letters
+            |> Result.bind has3ValidLetters
+            |> Result.map String.Concat
+            |> Result.map ValidRna 
+
+    let value (ValidRna rna) = rna
+
+let split (s:string) =
+    s
+    |> Seq.chunkBySize 3
+    
+let mapToProtein rna =
+    match Rna.value rna with
+    | "AUG" ->  "Methionine"
+    | _ -> "invalid"
+
 let proteins rna =
-    List.init 1 (fun _ -> "Methionine")
+    let result =
+        match Rna.create rna with
+        | Ok x -> x |> mapToProtein
+        | Error _ -> "ups"
+    
+    List.init 1 (fun _ -> result)
+    
