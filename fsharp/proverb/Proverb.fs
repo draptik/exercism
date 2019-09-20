befbfb6bfda2e27280bc02357a7707eb4b06f6ac
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-21 00:26:53 +0200

    fsharp: proverb
    - folding my way to success

--- fsharp/proverb/Proverb.fs
+++ fsharp/proverb/Proverb.fs
@@ -1,3 +1,37 @@
 ﻿module Proverb
 
-let recite (input: string list): string list = failwith "You need to implement this function."
+let getProverbFinalItem (allItems: string list) =
+    allItems.Head
+
+let createLastLine s =
+    sprintf "And all for the want of a %s." s
+
+let createLine want loss =
+    sprintf "For want of a %s the %s was lost." want loss
+
+let createLines finalItem items =
+    
+    let itemsWithPreviousItem =
+      List.fold
+          (fun state currentItem ->
+            let (previousLines, previousItem) = state
+            if currentItem = previousItem then
+                (previousLines, currentItem)
+            else                
+                let line = createLine previousItem currentItem
+                let lines = line :: previousLines
+                (lines, currentItem)
+          )
+          ([], finalItem)
+          items
+
+    let (list, _) = itemsWithPreviousItem
+    list
+    
+let recite (input: string list): string list =
+    match input with
+    | [] -> []
+    | items ->
+        let lastLine = getProverbFinalItem items |> createLastLine
+        let lines = createLines (getProverbFinalItem items) items
+        lastLine :: lines |> List.rev
