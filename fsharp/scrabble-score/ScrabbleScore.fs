a3e7f95ada467a8a055c66313938322d488609bf
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-20 21:40:53 +0200

    fsharp: scrabble score done

--- fsharp/scrabble-score/ScrabbleScore.fs
+++ fsharp/scrabble-score/ScrabbleScore.fs
@@ -1,3 +1,21 @@
 ﻿module ScrabbleScore
 
-let score word = failwith "You need to implement this function."
+open System
+
+let letterValue character =
+    match character with
+    | 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' -> 1
+    | 'D' | 'G' -> 2
+    | 'B' | 'C' | 'M' | 'P' -> 3
+    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+    | 'K' -> 5
+    | 'J' | 'X' -> 8
+    | 'Q' | 'Z' -> 10
+    | _ -> 0
+    
+let score word =
+    word
+    |> seq
+    |> Seq.map Char.ToUpper
+    |> Seq.map letterValue
+    |> Seq.sum
