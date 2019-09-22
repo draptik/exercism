34e13ced8800071f96e357c2f58b1c035859a0ad
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-22 14:06:51 +0200

    fsharp: anagram

--- fsharp/anagram/Anagram.fs
+++ fsharp/anagram/Anagram.fs
@@ -1,3 +1,17 @@
 ﻿module Anagram
+open System
 
-let findAnagrams sources target = failwith "You need to implement this function."
+let sort (s: string) =
+    s
+    |> seq
+    |> Seq.sort
+    |> String.Concat
+
+let isAnagram (s1: string) (s2: string) =
+    let s1' = s1.ToLowerInvariant()
+    let s2' = s2.ToLowerInvariant()
+    not (s1' = s2') && (sort s1') = (sort s2')
+
+let findAnagrams sources target =
+    sources
+    |> List.filter (isAnagram target)
