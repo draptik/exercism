da1592d94e96cc78d47df86ca7fc00587ef582fc
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-22 13:17:24 +0200

    fsharp: strain: example with recursion, fold, and foldBack

--- fsharp/strain/Strain.fs
+++ fsharp/strain/Strain.fs
@@ -1,15 +1,44 @@
 ﻿module Seq
 
-//let check f acc =
-//    let rec checkImpl f acc = function
-//        | [] -> []
-//        | h::t -> checkImpl f (acc @ [f h]) t
-//    checkImpl f [] acc
-//
-//check (fun x -> x * x) [1; 2; 3]
-    
-let keep pred xs = xs
-//    check pred xs    
-        
+let cheating pred xs =
+    xs |> Seq.filter pred
 
-let discard pred xs = failwith "You need to implement this function."
+// https://stackoverflow.com/a/32812776/1062607
+// NOT tail recursive!
+let rec filterRecursive f = function
+    | [] -> []
+    | head::tail ->
+        if f head then head :: filterRecursive f tail
+        else filterRecursive f tail
+
+// https://stackoverflow.com/a/32815491/1062607
+let filterFoldBack pred xs =
+    List.foldBack
+        (fun x acc ->
+            if pred x then x::acc
+            else acc)
+        xs
+        []
+
+let filterFold pred xs =
+    List.fold
+        (fun acc x  ->
+            if pred x then x::acc
+            else acc)
+        []
+        xs
+    |> List.rev
+
+let keep pred xs =
+//    cheating pred xs // using built-in `filter` function
+
+    xs
+    |> seq
+    |> Seq.toList
+//    |> filterRecursive pred // version 1
+//    |> filterFoldBack pred // version 2
+    |> filterFold pred // version 3
+
+let discard pred xs =
+    let negatedPredicate = pred >> not
+    keep negatedPredicate xs
