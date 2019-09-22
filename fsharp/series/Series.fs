130449c45e22778a3f1e5df69cd3a0ffd1134e99
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-22 15:28:32 +0200

    fsharp: series kata: first iteration

--- fsharp/series/Series.fs
+++ fsharp/series/Series.fs
@@ -1,4 +1,41 @@
 ﻿module Series
 
+open System
+
+let validateSeries data =
+    let (series, _) = data
+    if String.IsNullOrEmpty(series) then None
+    else Some data
+    
+let validateSliceLength data =
+    let (series: string, slice) = data
+    if series.Length < slice then None
+    else Some data
+
+let validatePositiveSliceLength data =
+    let (_, slice) = data
+    if slice > 0 then Some data
+    else None
+
+let createSubSeries data =
+    let (series: string, slice) = data
+    series
+    |> Seq.windowed slice
+    |> Seq.map (fun chars -> String.Concat(chars))
+    |> Seq.toList
+    |> Some
+
 let slices str length =
-    Some ["1"]
+//    let optionalInputValidation =
+//        validateSeries (str, length)
+//        |> Option.bind validateSliceLength
+//        |> Option.bind validatePositiveSliceLength
+//        
+//    match optionalInputValidation with
+//    | None -> None
+//    | Some data -> createSubSeries data |> Some
+
+    validateSeries (str, length)
+    |> Option.bind validateSliceLength
+    |> Option.bind validatePositiveSliceLength
+    |> Option.bind createSubSeries
