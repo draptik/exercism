24b5d44601e33043c4d25a9a1f7d5ec61952d617
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-26 00:02:53 +0200

    fsharp: clock - rename fullUnitMinutes ot timePeriodInMinutes - normalize with timePeriod divisor - simplify add

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -8,25 +8,23 @@ type Clock =
 
 let convertToTotalMinutes hours minutes = (hours * 60) + minutes
 
-let normalizeHour (minute: int) =  abs minute % 1440
-
-let normalizeMinute (minute: int) = abs minute % 60
+let normalize timePeriodInMinutes minute = abs minute % timePeriodInMinutes
 
 let toAnalogHour minute = minute / 60 % 24
 
 let toAnalogMinute minute = minute % 60
 
-let convertToAnalog fullUnitMinutes normalizingFunction toAnalogFunction totalMinutes =
-    let analogMinutes = normalizingFunction totalMinutes
+let convertToAnalog timePeriodInMinutes toAnalogFunction totalMinutes =
+    let analogMinutes = normalize timePeriodInMinutes totalMinutes 
     if totalMinutes < 0 then
-        fullUnitMinutes - analogMinutes |> toAnalogFunction 
+        timePeriodInMinutes - analogMinutes |> toAnalogFunction 
     else
         analogMinutes |> toAnalogFunction
     
 let convertToAnalogClock totalMinutes =
     {
-        Hour = totalMinutes |> convertToAnalog 1440 normalizeHour toAnalogHour
-        Minute = totalMinutes |> convertToAnalog 60 normalizeMinute toAnalogMinute
+        Hour = totalMinutes |> convertToAnalog 1440 toAnalogHour
+        Minute = totalMinutes |> convertToAnalog 60 toAnalogMinute
     }
 
 let create hours minutes =
@@ -34,8 +32,7 @@ let create hours minutes =
     |> convertToAnalogClock
 
 let add minutes clock =
-    let previousTotalMinutes = (clock.Hour * 60) + clock.Minute
-    previousTotalMinutes + minutes
+    convertToTotalMinutes clock.Hour clock.Minute + minutes
     |> convertToAnalogClock 
 
 let subtract minutes clock = add -minutes clock
