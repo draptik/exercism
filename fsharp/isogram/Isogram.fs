955788436637865301391ac01ab88cfd86c7c8e5
Patrick Drechsler <patrick@pdrechsler.de>
2019-09-26 01:40:18 +0200

    fsharp: isogram - V2: tolower -> sort -> distinct -> compare length

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,8 +1,24 @@
 ﻿module Isogram
 
 open System
+open System.Text.RegularExpressions
 
-let isIsogramInternalV1 (str : string) =
+let isIsogramInternalV2 (str: string) =
+
+    let toLetters (s: string) =
+        s
+        |> Seq.map Char.ToLowerInvariant
+        |> Seq.filter Char.IsLetter
+
+    let inputLength = str |> toLetters |> Seq.length 
+    
+    str
+    |> toLetters
+    |> Seq.sort
+    |> Seq.distinct
+    |> Seq.length = inputLength 
+    
+let isIsogramInternalV1 (str: string) =
     str
     |> Seq.map Char.ToLowerInvariant // map all strings to lowercase char array
     |> Seq.filter Char.IsLetter // we only care about letters
@@ -14,4 +30,4 @@ let isIsogramInternalV1 (str : string) =
     |> List.length = 0 // length > 0 means we have duplicates (not an isogram)
 
 let isIsogram str =
-    isIsogramInternalV1 str
+    isIsogramInternalV2 str
