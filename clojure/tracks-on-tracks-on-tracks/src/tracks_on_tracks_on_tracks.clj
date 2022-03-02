7a3564b67a32183df148b411d712644153a3b62d
Patrick Drechsler <patrick@pdrechsler.de>
2022-03-02 22:46:59 +0100

    clojure: list example

--- clojure/tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
+++ clojure/tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
@@ -3,26 +3,31 @@
 (defn new-list
   "Creates an empty list of languages to practice."
   []
+  ()
   )
 
 (defn add-language
   "Adds a language to the list."
   [lang-list lang]
+  (conj lang-list lang)
   )
 
 (defn first-language
   "Returns the first language on the list."
   [lang-list]
+  (first lang-list)
   )
 
 (defn remove-language
   "Removes the first language added to the list."
   [lang-list]
+  (rest lang-list)
   )
 
 (defn count-languages
   "Returns the total number of languages on the list."
   [lang-list]
+  (count lang-list)
   )
 
 (defn learning-list
@@ -30,4 +35,13 @@
   Java and JavaScript, then finally returns a count of the total number
   of languages."
   []
+  (-> (new-list)
+      (add-language "Clojure")
+      (add-language "Lisp")
+      (reverse)
+      (remove-language)
+      (add-language "JavaScript")
+      (add-language "Java")
+      (count)
+      )
   )
