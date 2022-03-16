e39b838a017f0c0a3f2f00827e51b50e9ca5cb5b
Patrick Drechsler <patrick@pdrechsler.de>
2022-03-16 23:41:25 +0100

    clojure: add sublist exercise

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -2,4 +2,22 @@
 
 (defn classify [list1 list2] ;; <- arglist goes here
       ;; your code goes here
-)
+
+  (defn sublist? [coll1 coll2]
+    (some #{coll1} (partition (count coll1) 1 coll2)))
+
+  ;; `defn-` defines a private function
+  (defn- isEmpty [list]
+    (= list []))
+
+  (cond
+    (= list1 list2) :equal
+    (sublist? list1 list2) :sublist
+    (sublist? list2 list1) :superlist
+    :else :unequal))
+  ;(if
+  ;  (and
+  ;    (isEmpty list1)
+  ;    (isEmpty list2))
+  ;  :equal
+  ;  :sublist))
