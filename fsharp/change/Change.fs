4edf4f92554cbfe8b4949a7a38d86946d91d3bfb
Patrick Drechsler <patrick.drechsler@mathema.de>
2022-03-30 16:43:13 +0200

    wip

--- fsharp/change/Change.fs
+++ fsharp/change/Change.fs
@@ -8,24 +8,36 @@
 //        )
 
 // http://howardism.org/Technical/LP/coin-kata.html
-let rec f (money : int) (coins : int list) : int =
-    let availableCoins = coins |> List.filter (fun x -> x <= money)
+//let rec f (money : int) (coins : int list) : int =
+//    let availableCoins = coins |> List.filter (fun x -> x <= money)
+//    
+//    if money = 0 then
+//        1
+//    else if money < 0 then
+//        0
+//    else if availableCoins.Length = 0 then
+//        0
+//    else
+//        f (money - coins.Head) coins + f money coins.Tail
+//
+//let countChange money coins =
+//    if money = 0 then
+//        0
+//    else
+//        f money (coins |> List.rev)
+
+let f coins target =
+    let sortedCoins = coins |> List.sortDescending
+    let folder (accAmount, result) coin =
+        let remainAmount = accAmount % coin
+        let coinClones = accAmount / coin |> List.replicate <| coin
+        remainAmount, coinClones @ result
+    let init = target, []
+    let tmp = List.fold folder init sortedCoins
     
-    if money = 0 then
-        1
-    else if money < 0 then
-        0
-    else if availableCoins.Length = 0 then
-        0
-    else
-        f (money - coins.Head) coins + f money coins.Tail
-
-let countChange money coins =
-    if money = 0 then
-        0
-    else
-        f money (coins |> List.rev)
-
+    
+    
+    
 let findFewestCoins coins target =
 
     if List.contains target coins then
