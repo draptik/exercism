51ead7f394738820a308b65137b5cad6d0113d16
Patrick Drechsler <patrick@pdrechsler.de>
2022-03-30 18:36:11 +0200

    fsharp: change works
    This isn't my solution (see comment for source). At least I understand how it works...

--- fsharp/change/Change.fs
+++ fsharp/change/Change.fs
@@ -1,31 +1,9 @@
 ﻿module Change
 
 // https://codereview.stackexchange.com/questions/155967/coin-change-kata-in-immutablejs
-//let foo denominations amount =
-//    denominations
-//    |> List.reduce (fun acc elem ->
-//        let remainder = acc
-//        )
-
 // http://howardism.org/Technical/LP/coin-kata.html
-//let rec f (money : int) (coins : int list) : int =
-//    let availableCoins = coins |> List.filter (fun x -> x <= money)
-//    
-//    if money = 0 then
-//        1
-//    else if money < 0 then
-//        0
-//    else if availableCoins.Length = 0 then
-//        0
-//    else
-//        f (money - coins.Head) coins + f money coins.Tail
-//
-//let countChange money coins =
-//    if money = 0 then
-//        0
-//    else
-//        f money (coins |> List.rev)
 
+// This will only work if coins are multiples of each other
 let f coins target =
     let sortedCoins = coins |> List.sortDescending
     let folder (accAmount, result) coin =
@@ -33,14 +11,46 @@ let f coins target =
         let coinClones = accAmount / coin |> List.replicate <| coin
         remainAmount, coinClones @ result
     let init = target, []
-    let tmp = List.fold folder init sortedCoins
+    let remain, cs = List.fold folder init sortedCoins
+    match remain, cs with
+    | 0, [] -> None
+    | 0, _ -> Some cs
+    | _, _ -> None
+
+// https://exercism.org/tracks/fsharp/exercises/change/solutions/11e5c0c8a72946a0b65b295da5a7cef8
+let change target coins =
     
+    let tryAddToPocket pocket coin =
+        let changeToReturn = target - List.sum pocket
+        
+        if coin > changeToReturn then
+            None
+        else
+            coin :: pocket |> List.sort |> Some
+        
+    let pocketsWithExtraCoin pocket =
+        coins 
+        |> List.choose (tryAddToPocket pocket)
+        
+    let rec loop pockets =
+        let targetReached = 
+            pockets 
+            |> List.tryFind (List.sum >> (=) target)
+        
+        match targetReached with
+        | Some p -> 
+            p 
+            |> List.sort 
+            |> Some
+        | None when List.isEmpty pockets -> 
+            None
+        | None ->
+            pockets
+            |> List.distinct
+            |> List.collect pocketsWithExtraCoin
+            |> loop
     
-    
+    loop [[]]
     
 let findFewestCoins coins target =
-
-    if List.contains target coins then
-        Some [target]
-    else
-        None
+    change target coins
