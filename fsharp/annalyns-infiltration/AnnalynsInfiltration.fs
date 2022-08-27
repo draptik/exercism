d6bf893dffbc5e53e67dadf0136ebe436ea2ff13
Patrick Drechsler <patrick@pdrechsler.de>
2022-08-27 22:55:54 +0200

    fsharp: solve annalyns infiltration exercise

--- fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
+++ fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
@@ -1,12 +1,20 @@
 module AnnalynsInfiltration
 
-let canFastAttack (knightIsAwake: bool): bool = failwith "Please implement the 'canFastAttack' function"
-
+let canFastAttack (knightIsAwake: bool): bool =
+    knightIsAwake |> not
+    
 let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSpy' function"
-
+    [knightIsAwake; archerIsAwake; prisonerIsAwake] |> List.contains true
+    
 let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSignalPrisoner' function"
+    let archerIsSleeping = archerIsAwake |> not 
+    archerIsSleeping && prisonerIsAwake
 
 let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool): bool =
-    failwith "Please implement the 'canFreePrisoner' function"
+    let archerIsSleeping = archerIsAwake |> not
+    let knightIsSleeping = knightIsAwake |> not
+    if petDogIsPresent && archerIsSleeping then
+        true
+    else
+        prisonerIsAwake && archerIsSleeping && knightIsSleeping
+        
